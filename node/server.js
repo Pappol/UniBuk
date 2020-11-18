12020df64230df81951b49af24ab20456c2a2228
riccardo <riccardo.peron@studenti.unitn.it>
2020-11-18 14:34:41 +0100

    :eyes: add demo book get request

--- node/server.js
+++ node/server.js
@@ -5,6 +5,17 @@ const app = express();
 
 // Log packet
 app.use(morgan('dev'))
+app.use((req, res, next) => {
+  //set degli headers (non invia la risposta)
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Request-Width, Content-Type, Accept, Authorization');
+  //OPTIONS è un metodo di controllo per vedere se puoi fare un certo tipo di richiesta
+  if(req.method === 'OPTIONS'){
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+      return res.status(200).json({});
+  }
+  next();
+});
 
 // Constants
 const PORT = 8080;
@@ -12,6 +23,19 @@ const HOST = '0.0.0.0';
 
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+app.get('/:boooks', (req, res) => {
+  res.status(200).json(
+    {
+      books: [
+        {isbn: '1234', title: 'Dragon Book', author: 'qu4gl14', year: '1964', editor: 'boh', description: 'lfc'},
+        {isbn: '1234', title: 'Dragon Book', author: 'qu4gl14', year: '1964', editor: 'boh', description: 'lfc'},
+        {isbn: '1234', title: 'Dragon Book', author: 'qu4gl14', year: '1964', editor: 'boh', description: 'lfc'},
+        {isbn: '1234', title: 'Dragon Book', author: 'qu4gl14', year: '1964', editor: 'boh', description: 'lfc'}
+      ]
+    }
+  );
 });
 
 app.listen(PORT, HOST);
