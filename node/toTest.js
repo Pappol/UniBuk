bca8b44ce1e073f26273edcefcce28795da7ea58
Pappol <riccardo.parola@studenti.unitn.it>
2020-11-19 14:59:53 +0100

    :hammer: Bug fixed Travsi CI

--- node/toTest.js
+++ node/toTest.js
@@ -1,3 +1,9 @@
+const axios = require('axios');
+
 const toTest = {
-    add:(num1, num2) => num1+num2
+    add:(num1, num2) => num1+num2,
+    fetchUser: () => axios.get('https://jsonplaceholder.typicode.com/users/1')
+    .then(res => res.data)
+    .catch(err => 'Error with the get')
 }
+module.exports = toTest;
--- node/toTest.test.js
+++ node/toTest.test.js
@@ -4,3 +4,9 @@ const toTest = require('./toTest');
 test('Adds 2+2 equal 4', () => {
     expect(toTest.add(2,2)).toBe(4);
 });
+
+test('Get funciont', async () => {
+    expect.assertions(1);
+    const data = await toTest.fetchUser();
+    expect(data.name).toEqual('Leanne Graham');
+});
