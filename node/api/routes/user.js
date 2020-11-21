5948792d58d6d12fc70d5c54f6e95feaec7b1530
filippodaniotti <febbrosto@gmail.com>
2020-11-21 08:22:12 +0100

    :memo: Added GET user api

--- node/api/routes/user.js
+++ node/api/routes/user.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const UserController = require('../controllers/user');
 const checkAuth = require('../middlewares/check-auth');
 
+router.get('/:userId', UserController.users_get_user);
 router.post('/signup', UserController.user_signup);
 router.post('/login', UserController.user_login);
 // router.patch('/:userId', UserController.user_update);
