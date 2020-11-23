dd9f3ff8f7342e68fad5acf10db53bde7fc9dace
riccardo <riccardo.peron@studenti.unitn.it>
2020-11-23 15:28:50 +0100

    :eyes: Added react-bootstrap

--- app/src/components/Auth/login.jsx
+++ app/src/components/Auth/login.jsx
@@ -1,52 +1,87 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom';
 import Axios from 'axios';
+
+import Form from 'react-bootstrap/Form'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Button from 'react-bootstrap/Button'
 
 export class Login extends Component {
 
-    handleSubmit = e => {
-        e.preventDefault();
-        const data = {
-            email: this.email,
-            password: this.password
-        }
-        console.table(data);
-        Axios.post('http://localhost:8080/user/login', data)
-            .then(res => {
-                console.log(res);
-                localStorage.setItem('token', res.token);
-            })
-            .catch(err => {
-                console.log(err);
-            });
+  handleSubmit = e => {
+    e.preventDefault();
+    const data = {
+      email: this.email,
+      password: this.password
     }
+    console.table(data);
+    Axios.post('http://localhost:8080/user/login', data)
+      .then(res => {
+        console.log(res);
+        localStorage.setItem('token', res.token);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <h3>Sign In</h3>
+  render() {
 
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" onChange={e => this.email = e.target.value}/>
-                </div>
+    return (
+      <Jumbotron className = 'mx-auto mt-5'>
 
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" onChange={e => this.password = e.target.value}/>
-                </div>
+        <h3>SignIn</h3>
 
-                {/* <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                    </div>
-                </div> */}
+        <Form onSubmit={this.handleSubmit}>
 
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
-                {/* <p className="forgot-password text-right">
-                    Forgot <a href="#">password?</a>
-                </p> */}
-            </form>
-        );
-    }
+          <Form.Group controlId="formGroupEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" onChange={e => this.email = e.target.value}/>
+          </Form.Group>
+          
+          <Form.Group controlId="formGroupPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" onChange={e => this.password = e.target.value}/>
+          </Form.Group>
+
+          <Button variant="primary" type="submit" block>
+            Sign in
+          </Button>
+
+          <br />
+          <Link to = '/user/signup' className = 'text-primary'>Don't have an account? Sign-up</Link>
+
+        </Form>
+      </Jumbotron>
+    )
+
+    //        return (
+    //            <form onSubmit={this.handleSubmit}>
+    //                <h3>Sign In</h3>
+    //
+    //                <div className="form-group">
+    //                    <label>Email address</label>
+    //                    <input type="email" className="form-control" placeholder="Enter email" onChange={e => this.email = e.target.value}/>
+    //                </div>
+    //
+    //                <div className="form-group">
+    //                    <label>Password</label>
+    //                    <input type="password" className="form-control" placeholder="Enter password" onChange={e => this.password = e.target.value}/>
+    //                </div>
+    //
+    //                {/* <div className="form-group">
+    //                    <div className="custom-control custom-checkbox">
+    //                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
+    //                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+    //                    </div>
+    //                </div> */}
+    //
+    //                <button type="submit" className="btn btn-primary btn-block">Submit</button>
+    //                {/* <p className="forgot-password text-right">
+    //                    Forgot <a href="#">password?</a>
+    //                </p> */}
+    //            </form>
+    //        );
+
+  }
 }
