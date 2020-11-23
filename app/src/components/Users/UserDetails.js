33ef0f79c36f93b1da40dfd6221bd299811a5cc6
riccardo <riccardo.peron@studenti.unitn.it>
2020-11-23 22:54:30 +0100

    :memo: Add user details page

---
+++ app/src/components/Users/UserDetails.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Jumbotron from 'react-bootstrap/esm/Jumbotron';
+
+import Container from 'react-bootstrap/Container'
+import Switch from 'react-bootstrap/esm/Switch';
+
+export default class UserDetails extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      resource: {}
+    }
+  }
+
+  async componentDidMount() {
+    const { match } = this.props;
+    console.log(this.props)
+    const res = await fetch(`http://localhost:8080/user/${match.params.userId}`);
+    const json = await res.json();
+    this.setState({
+      resource: json.user,
+    });
+  }
+
+  render() {
+    const { resource } = this.state;
+    return (
+        <Jumbotron className = 'mx-5 my-5'>
+          <h3> Profilo Utente </h3>
+          <h4>UserDetails</h4>
+          <p>e-mail: {resource.email}</p>
+          <h4>Student Creeds</h4>
+        </Jumbotron>      
+    )
+  }
+}
