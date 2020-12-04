e4c03808f2bfb1a419d997317bb937f4245f895b
MarcoDiFrancesco <marcodifran@gmail.com>
2020-12-04 14:16:58 +0100

    :memo: Add web deployment to travis

--- publish-to-heroku.sh
+++ publish-to-heroku.sh
@@ -13,3 +13,18 @@ curl --netrc -X PATCH https://api.heroku.com/apps/$YOUR_HEROKU_APP/formation \
   -H "Content-Type: application/json" \
   -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
   -H "Authorization: Bearer $DOCKER_PASSWORD"
+
+YOUR_HEROKU_APP=unibuk
+DOCKER_IMAGE_ID=$(docker inspect registry.heroku.com/$YOUR_HEROKU_APP/web --format={{.Id}})
+curl --netrc -X PATCH https://api.heroku.com/apps/$YOUR_HEROKU_APP/formation \
+  -d '{                                                                               
+  "updates": [
+    {
+      "type": "web",
+      "docker_image": "'"$DOCKER_IMAGE_ID"'"
+    }
+  ]
+}' \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
+  -H "Authorization: Bearer $DOCKER_PASSWORD"
