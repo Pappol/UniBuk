e448c9040b66b6b5563bd4c1fd23edf64a5abe2b
RiccardoPeron <riccardo.peron@studenti.unitn.it>
2020-12-11 22:13:32 +0100

    :hammer: separed getFavourites and setFavourites

--- app/src/components/Resources/Resources.js
+++ app/src/components/Resources/Resources.js
@@ -17,11 +17,13 @@ class Resources extends Component {
     this.state = {
       type: "books",
       resources: [],
+      favourites: []
     };
   }
 
   componentDidMount() {
     this.getResources(localStorage.kind || "books");
+    this.getFavourites();
   }
 
   getResources = async (kind) => {
@@ -42,34 +44,41 @@ class Resources extends Component {
     }
   };
 
-  getFavourites = async (kind) => {
+  getFavourites = async () => {
     if(localStorage.myId === null || localStorage.myId === '') {
       this.setState({
-        resources: null,
+        favourites: null,
       });
     } else {
       const usrRes = await fetch(`${process.env.REACT_APP_BACKEND_URL}/user/${localStorage.myId}`);
       const json = await usrRes.json();
       const favs = await json.user.favourites;
-      let favResources = [];
-
-      for( const fav of favs) {
-        const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/${kind}/${fav}`);
-        const jres = await res.json();
-
-        if(typeof(jres.message) === 'undefined' && kind === 'books') {
-          favResources.push(jres.book);
-        }
-
-        if(typeof(jres.message) === 'undefined' && kind === 'contents') {
-          favResources.push(jres.content);
-        }
-      }
       this.setState({
-        resources: favResources
+        favourites: favs
       });
     }
   };
+
+  setFavourites = async (kind) => {
+    const favs = this.state.favourites;
+    let favResources = [];
+
+    for( const fav of favs) {
+      const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/${kind}/${fav}`);
+      const jres = await res.json();
+
+      if(typeof(jres.message) === 'undefined' && kind === 'books') {
+        favResources.push(jres.book);
+      }
+
+      if(typeof(jres.message) === 'undefined' && kind === 'contents') {
+        favResources.push(jres.content);
+      }
+    }
+    this.setState({
+      resources: favResources
+    });
+  }
 
   // getResourcePage = () => {
   //   if (this.state.type === 'book') {
@@ -93,15 +102,11 @@ class Resources extends Component {
             <Button variant="primary" className = 'mr-2 mb-2' onClick={() => this.getResources("contents")}>
               Contenuti
             </Button>
-            <Button variant = 'primary' className = 'mr-2 mb-2' onClick = {() => this.getFavourites(this.state.type+'s')}>
+            {this.state.favourites == null ? null :
+            <Button variant = 'primary' className = 'mr-2 mb-2' onClick = {() => this.setFavourites(localStorage.kind)}>
               {this.state.type} Salvati
             </Button>
-            {resources == null ?
-              <Jumbotron>
-                Che credevi di trovare andando a caccia di cervi in mare aperto?<br />
-                Prova ad andare <Link to = '/user/login'>in montagna</Link>
-              </Jumbotron>:
-
+            }
               <Row>
                 {resources.map(resource => (
                   <Col className="col-sm-12 col-md-6 col-lg-4" key={resource._id}>
@@ -109,7 +114,6 @@ class Resources extends Component {
                   </Col>
                 ))}
               </Row>
-            }
           </Container>
         </Route>
         <Route exact path={`${this.props.match.path}/:resourceId`}>
